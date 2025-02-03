77cbfdbba661e760777f3076abe45cf1a4d005be
amalali44 <108816584+amalali44@users.noreply.github.com>
2025-02-03 03:27:12 +0000

    02/02/2025

---
+++ A1/countingCovid.c
@@ -0,0 +1,8 @@
+/*
+Name: Amal Ali
+Assignment #1
+Lab Section: X06L
+Lab Instructor’s Name: Dhara wagh
+Lecture Instructor’s Name: Ardy RD
+*/
+
---
+++ A1/squeeze.c
@@ -0,0 +1,29 @@
+/*
+Name: Amal Ali
+Assignment #1
+Lab Section: X06L
+Lab Instructor’s Name: Dhara wagh
+Lecture Instructor’s Name: Ardy RD
+*/
+
+#include <stdio.h>
+
+int squeeze() 
+{
+    char k;
+    printf("Please enter a sequence of letters: %c", k);
+    scanf('');
+    
+    printf("\n\n\n\n this is what you typed");
+    //printf({STRING}(_k));
+
+    int counter= 0;
+
+    return 0;   
+}
+
+int main()
+{
+    squeeze();
+    return 0;
+}
