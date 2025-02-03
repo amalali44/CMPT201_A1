0a3dae375751770ff5207c698e6a7e732470aca0
amalali44 <108816584+amalali44@users.noreply.github.com>
2025-02-03 18:06:52 +0000

    02/03/2025

--- A1/squeeze.c
+++ A1/squeeze.c
@@ -1,25 +1,55 @@
-/*
-Name: Amal Ali
+/*------------------------------------------
+Student’s Name: Amal Ali
 Assignment #1
 Lab Section: X06L
-Lab Instructor’s Name: Dhara wagh
+Lab Instructor’s Name: Dhara Wagh
 Lecture Instructor’s Name: Ardy RD
-*/
-
+--------------------------------------------*/
 #include <stdio.h>
 
-int squeeze() 
+void squeeze()
 {
-    char k;
-    printf("Please enter a sequence of letters: %c", k);
-    scanf('');
+    char string_input[100];
+    printf("Enter a sequence of letters: ");
+    scanf("%s", string_input);
+
+    int counter = 1;
+    char prev_char = string_input[0];
+
+    int i = 1;
+    while (string_input[i] != 0)
+    {
+        if (string_input[i] == prev_char)
+        {
+            counter++;
+            if (counter == 9)
+            {
+                printf("%c%d", prev_char, counter);
+                counter = 0;
+            }
+        }
+        else
+        {
+            if (counter > 1)
+                printf("%c%d", prev_char, counter);
+            
+            if (counter == 1)
+                printf("%c", prev_char);
+        
+
+            prev_char = string_input[i];
+            counter = 1;
+        }
+        i++;
+    } 
+
+    if (counter > 1)
+        printf("%c%d", prev_char, counter);
+    if (counter == 1)
+        printf("%c", prev_char);
+
+    printf("\n");
     
-    printf("\n\n\n\n this is what you typed");
-    //printf({STRING}(_k));
-
-    int counter= 0;
-
-    return 0;   
 }
 
 int main()
