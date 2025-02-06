1cf58e8c54ff6ec2355536c15b46fab6321c5b4a
amalali44 <108816584+amalali44@users.noreply.github.com>
2025-02-06 21:35:37 +0000

    02/06/2025

--- A1/xTable.c
+++ A1/xTable.c
@@ -6,3 +6,45 @@ Lab Instructor’s Name: Dhara wagh
 Lecture Instructor’s Name: Ardy RD
 */
 
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // int x, y;
+
+    // printf("Pick a num: ");
+    // scanf("%d", &x);
+
+    // printf("Pick another num: ");
+    // scanf("%d", &y);
+
+    // Print column headers
+    printf(" + ");
+    int col = 5;
+    while (col <= 100) {
+        printf("\t%d", col);
+        col += 5;
+    }
+    printf("\n");
+
+    // Print table with calculations
+    int row = 5;
+    while (row <= 100) {
+        printf("%d\t", row);  // Row header
+        
+        col = 5;  // Reset col for each row
+        while (col <= 100) {
+            double eqn = (pow(col, 4) / pow(row, 4)) + sqrt(row);
+            printf("%.1f\t", eqn);
+            col += 5;
+        }
+        printf("\n");
+        row += 5;
+    }
+
+    return 0;
+}
+
+    
+    
+    
