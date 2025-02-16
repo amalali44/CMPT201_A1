7d0d1d40b8e734c56427f6dc6fda11c25a4d7976
amalali44 <108816584+amalali44@users.noreply.github.com>
2025-02-16 01:35:03 +0000

    02/15/2025

--- A1/Testing_directory/README.md
+++ A1/Testing_directory/README.md
@@ -1,11 +1,61 @@
-# CMPT201-A1
-CMPT 201- Assignment 1
+# Name: Amal Ali
+# Assignment #1
+# Lab Section: X06L
+# Lab Instructor’s Name: Dhara wagh
+# Lecture Instructor’s Name: Ardy RD
 
 
 Program 1 squeeze.c :
+-> This program compresses repeating characters in a string by replacing them with the char followed by its count. If a character repeats more than 9 times, the program splits it into multiple groups of 9 or fewer. Exits with crtl+d
+
+Example input: aaaaaabbcdd
+Expected output: a6b2cd2
+
+
+#------------------------------------------------------------------------------------
 
 program 2 xTable.c :
+-> This program prints the result of a math equation ((x^4 / y^4) + sqrt(y)) in tabular form for integers countng from 5 to 100, in steps of five. 
+
+Example input: ./xTable
+Expected output: 
+
+(the top row and column extend to 100)
+
+         +     5       10      15   .   .   . 
+        5     3.2     18.2    83.2
+        10    3.2     4.2     8.2
+        15    3.9     4.1     4.9
+        .
+        .
+        .
+
+
+#------------------------------------------------------------------------------------
 
 Program 3: CountingCovid.c : 
+-> counts the number of COVID-19 cases per date from a dataset. It reads data from standard input, ignores the header line, extracts the third column (after 2 commas) that has the date reported, and prints the date along with the number of cases on that date. Exits with crtl+d
+
+Example input: ./countingCovid < covidData_100.csv
+Expected output: 
+2020/03/05 1
+2020/03/07 1
+2020/03/08 1
+2020/03/09 4
+2020/03/10 9
+2020/03/11 7
+2020/03/12 3
+2020/03/13 8
+2020/03/14 22
+2020/03/15 11
+2020/03/16 24
+2020/03/17 8
+
+#------------------------------------------------------------------------------------
+
 
 Program 4: CardinalSort.c
+-> This program sorts chars (including spcases, punctuation and capital letters) in ascending order based on their ASCII value. Exits with crtl+d.
+
+Example input: $hello cmpt 201
+Expected output:   012cehllmopt
--- A1/countingCovid.c
+++ A1/countingCovid.c
@@ -7,86 +7,75 @@ Lecture Instructor’s Name: Ardy RD
 Purpose: reports the number of covid cases seen each day in a given dataset
 credit: 
 
-explained how to count occurrences in a text file, helped with my loops
-https://stackoverflow.com/questions/59232071/c-programming-occurrences-of-an-integers-in-an-text-file?utm_source=chatgpt.com
+explained how to counter occurrences in a text file, helped with my loops
+https://stacindex_koverflow.com/questions/59232071/c-programming-occurrences-of-an-integers-in-an-text-file?utm_source=chatgpt.com
 
 taught me how to use the sizeof operator
-https://www.geeksforgeeks.org/sizeof-operator-c/
+https://www.geeindex_ksforgeeindex_ks.org/sizeof-operator-c/
 
 taught me how to use the logical operators  ( &&, ||, ! )
-https://www.geeksforgeeks.org/logical-operators-in-c/
+https://www.geeindex_ksforgeeindex_ks.org/logical-operators-in-c/
 */
 
 
 #include <stdio.h>
 
-#define max_dates 300  
-#define len_of_date 11     
+#define MAX_LINE_LENGTH 256 
+#define LEN_OF_DATE 11 // yyyy/mm/dd + 1 extra for null
 
 int main() {
-    // char dates[max_dates][len_of_date];
-    int count[max_dates] = {}; 
-    int date_count = 0;
-    char line[256];
-    char date[len_of_date];
+    char line[MAX_LINE_LENGTH]; // buffer
+    char date_storage[LEN_OF_DATE] = ""; //  stores the current date being counted
+    int counter = 0; 
 
-    fgets(line, 256, stdin); // Skip the fist line which is the header
+    fgets(line, sizeof(line), stdin);  // Skip the first line which is the header line
 
-    // Read each subsequent line
-    while (fgets(line, 256, stdin)) {
+
+    while (fgets(line, sizeof(line), stdin)) {   // Processes each line 
         int i = 0, j = 0;
 
-        while (line[i] != ',' && line[i] != '\0') // Skip the first field
+        
+        while (line[i] != ',' && line[i] != '\0') // skip first comma separatedcoloumn
+            i++; 
+        i++;
+        while (line[i] != ',' && line[i] != '\0') // skips second comma separated coloumn
             i++;
         i++; 
+        
 
-        while (line[i] != ',' && line[i] != '\0') // Skip the second field
-            i++;
-        i++;
-
-
-        // use the third field that contains the date
+        char current_date[MAX_LINE_LENGTH] = ""; // stores the extracted current dates
         j = 0;
-        while (line[i] != ',' && j < len_of_date - 1) {
-            date[j++] = line[i++];
+
+        while (line[i] != ','  && j < MAX_LINE_LENGTH - 1) {
+            current_date[j++] = line[i++];
         }
-   
 
-        // // Check if the date already exists
-        // int exists = 0;
-        // int k = 0;
-        // while (k < date_count && !exists) {
-            
-        //     int a = 0;
-        //     while (a < len_of_date && dates[k][a] != '\0' && date[a] == dates[k][a]) {
-        //         a++; }
+        int exists = 1;
+        int index_k = 0;
 
-        //     if ( dates[k][a] == '\0') {  
-        //         count[k]++; // If the entire string matches, increment the count
-        //         exists = 1; }
-        //     k++;
-        // }
+        while (exists && (current_date[index_k] != '\0' || date_storage[index_k] != '\0')) { // compares each element date stoarage with current_date  
+            if (current_date[index_k] != date_storage[index_k]) { // if the current date is not in date storage (dates are not the same)
+                exists = 0; } // Set the flag to 0 
+            index_k++; 
+        }
 
-
-        // // If the date doesn't exist, add it to the date array
-        // if (!exists && date_count < max_dates) {
-        //     int m = 0;
-
-        //     while (m < len_of_date && date[m]) {
-        //         dates[date_count][m] = date[m];
-        //         m++;
-        //     }
-        //     dates[date_count][m] = '\0'; // Null-terminate the new date
-        //     count[date_count] = 1;
-        //     date_count++;
-        // }
+        if (!exists) { // If the current_date is different
+            if (counter > 0) {
+                printf("%s %d\n", date_storage, counter); //print the date and it's counter 
+            }
+            // Copy the new current_date into date_storage
+            for (int index_k = 0; current_date[index_k] != '\0'; index_k++) {
+                date_storage[index_k] = current_date[index_k];
+            }
+            counter = 1;
+        } else 
+            counter++;
+        
     }
 
-    // Print the dates and their count
-    int i = 0;
-    while (i < date_count) {
-        //printf("%s %d\n", dates[i], count[i]);
-        i++;
+    if (counter > 0) {
+        printf("%s %d\n", date_storage, counter);     // Print all the dates and their count
+
     }
 
     return 0;
