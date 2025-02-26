371e58d1990e440440731291d1c803e7916aaefb
amalali44 <108816584+amalali44@users.noreply.github.com>
2025-02-26 01:03:35 +0000

    02/25/2025

---
+++ A2/ht_impl.h
@@ -0,0 +1,20 @@
+/* ht_impl.h
+ *
+ * For any "private"  declarations that should not be visible to the public
+ * users of the hash table, but might want to be shared across various source
+ * files in the hash table implementation.
+ * 
+ * It is a common convention to append "_impl" to a private implementation of
+ * some public interface, so we do so here.
+ *
+ * Author: <TODO: your name here>
+ * Lab instructor: <TODO: Your lab instructor's name here>
+ * Lecture instructor: <TODO: Your lecture instructor's name here>
+ */
+
+#ifndef _HT_IMPL_H_
+#define _HT_IMPL_H_
+
+/* TODO: think of some things that should go in here! */
+
+#endif
